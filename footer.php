18c7725dd64f7046ac031e4816ee218ecc39968f
dina1122 <dinachatfe5@gmail.com>
2021-01-12 13:03:06 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+   <!--Footer-->
+   <div id="footer">
+       <div>
+           <p>CONTACT US</p>
+       </div>
+       <div>
+           <p>ABOUT</p>
+       </div>
+       <div>
+           <p>DEVELOPER</p>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facere quibusdam vero dolore atque iure
+               provident deleniti eius, et sit error culpa suscipit repellendus consequatur pariatur recusandae esse
+               distinctio maiores!</p>
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi unde mollitia vel. Quod ipsam dolores
+               similique asperiores impedit quibusdam mollitia voluptatum dolore nihil illum deleniti voluptates, animi
+               inventore quos eaque!</p>
+       </div>
+   </div>
